6f7aaf26491206be2a67905f87f644098eaa87ca
Adrian-Seg <asegura1306@yahoo.com>
2021-04-15 11:40:07 -0700

    Began HTML for schedule/route details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { SchedulesMapsComponent } from './pages/schedules-maps/schedules-maps.component';
 import { ServicesPageComponent } from './pages/services-page/services-page.component';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { ServicesPageComponent } from './pages/services-page/services-page.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoogleMapsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
