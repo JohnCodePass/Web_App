429e34d624109315ba62771f167302e5f8e51ce2
Matthew-J-Mendoza <mattjm112999@gmail.com>
2021-04-20 13:54:07 -0700

    Added functionality to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { ServicesPageComponent } from './pages/services-page/services-page.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'WebRTD';
   public isMenuCollapsed = true;
+  isServiceActive:string
+  isAboutActive:string
+  isSched_MapsActive:string
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      // see also 
+      if (val instanceof NavigationEnd ) {
+        console.log(val.url)
+        this.isServiceActive = val.url === "/service-page" ? "active" : ""
+        this.isAboutActive = val.url === "/About" ? "active" : ""
+        this.isSched_MapsActive = val.url === "/Schdules&Maps" ? "active" : ""
+      }
+  });
+   }
+
+  ngOnInit(): void {
+    console.log(window.location.pathname)
+  }
+  
+
 }
--- src/app/pages/services-page/services-page.component.ts
+++ src/app/pages/services-page/services-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { LocalStorageService } from 'src/app/services/localstorage/local-storage.service';
 
 @Component({
@@ -7,10 +7,19 @@ import { LocalStorageService } from 'src/app/services/localstorage/local-storage
   styleUrls: ['./services-page.component.css']
 })
 export class ServicesPageComponent implements OnInit {
+  
 
-  constructor(private lsService: LocalStorageService) { }
-
-  ngOnInit(): void {
+  constructor(private lsService: LocalStorageService) { 
+    
   }
 
+  ngOnInit(): void {
+    // switch(window.location.pathname){
+    //   case: ""
+
+    //   break;
+    // }
+
+  }
+ 
 }
